909e74074fbddfdb2e77d7845fb8dd002e0ed5e9
radityaIch <raditya2678@gmail.com>
2025-10-05 22:57:29 +0800

    feat: implement CSRF protection with proper configuration and exclude delete routes temporarily

--- application/config/config.php
+++ application/config/config.php
@@ -225,7 +225,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 2;
 
 /*
 |--------------------------------------------------------------------------
@@ -457,11 +457,11 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
-$config['csrf_regenerate'] = TRUE;
+$config['csrf_regenerate'] = FALSE;  // Changed to FALSE to prevent issues with AJAX requests
 $config['csrf_exclude_uris'] = array();
 
 /*
